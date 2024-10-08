a398d3419ab90cbb2b2782d35bb5239031f169f1
jigneshkumar1 <jigneshsolanki436@gmail.com>
2024-10-08 12:22:27 +0530

    add comment

--- Python Assignment/Fundamentals of python/Q1.py
+++ Python Assignment/Fundamentals of python/Q1.py
@@ -1,10 +1,13 @@
 # • Write a Python program to check if a number is positive, negative or 
 # zero. 
-
+#Took number input from the user  
 num = int(input("Enter the number: "))
+# check if the number is greater than zero
 if num > 0:
-    print("Number is positive")
+    print("Number is positive") # agr number zero se greater hai to print number is positive
+# check if the number is less than zero
 elif num < 0:
-    print("Number is negative")
+    print("Number is negative") # agr number zero se less hai to print number is negative
+#  if a number is neither greater than zero nor smaller than zero it is equl to zero
 else:
-    print("Number is zero")
+    print("Number is zero") # if the number equl to zero hai to else print hoga
