9812934ca01b9953634c98aaee50c16d08d7211d
jigneshkumar1 <165266367+jigneshkumar1@users.noreply.github.com>
2024-12-07 23:36:46 +0530

    Add files via upload

---
+++ Python Assignment/Advance Python Programming/1. Printing on Screen/Lab/Q1.py
@@ -0,0 +1,11 @@
+# • Write a Python program to print a formatted string using print() and f-string. 
+
+name = "Jignesh"
+id = 101
+
+# using print
+print(name,"your id number is",id)
+
+# using f-string
+print()
+print(f"{name} your id number is {id}")
